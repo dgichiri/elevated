b334b8acc8bb3e33afd3ead1314f8e2efb40dd84
dgichiri <dgichiri@gmail.com>
2024-11-03 10:28:32 +0300

    Update Elevate.py

--- Elevate.py
+++ Elevate.py
@@ -14,22 +14,10 @@ st.write("""
         
          """)
 
-msgs = [ "Together, we can build something incredible. Let's foster a spirit of collaboration and see how far we can go. Lucille",
-"Unity is strength. When we work together, we can move mountains. Let's leverage our unique strengths for the greater good. Lucille",
-"Remember, we're not just colleagues, we're a team. And as a team, we thrive. Let's make every day count. #Collaboration. Lucille",
-"In the face of challenges, we stand stronger together. Let's turn obstacles into opportunities. #TeamBuilding. Lucille",
-"Diversity is our strength. Let's celebrate our unique perspectives and work together to create a harmonious workspace. Lucille",
-"The whole is greater than the sum of its parts. Let's combine our skills and talents to achieve greatness. #TeamBuilding. Lucille",
-"Let's break down barriers and build bridges. Together, we can create a workspace that thrives on collaboration and mutual respect. Lucille",
-"Remember, we're all in this together. Let's support each other and work towards our common goals. #TeamBuilding. Lucille",
-"Our success is intertwined. Let's help each other grow and thrive in a collaborative environment. #Collaboration. Lucille",
-"Let's turn our differences into strengths. Together, we can create a dynamic and thriving team. #Bonding. Lucille",
-"The power of collaboration is endless. Let's work together to create a workplace that inspires and motivates us. Lucille",
-"Unity in diversity is our strength. Let's celebrate our unique perspectives and work together towards our common goals. Lucille",
-"Remember, we're not just a team, we're a family. And like a family, we should support and uplift each other. Lucille",
-"Let's foster a culture of collaboration and mutual respect. Together, we can achieve great things.Lucille",
-"Our success is a reflection of our teamwork. Let's work together to create a workplace that thrives on collaboration and unity. Lucille",
-"Let's break down silos and build bridges. Together, we can create a workspace that encourages collaboration and innovation. Lucille",
+msgs = [ "Welcome to the CO Fun Fest! Let's reconnect, rejuvenate and shine together
+          We also call ourselves team eagle
+          We soar high",
+
                      
         ]
 
